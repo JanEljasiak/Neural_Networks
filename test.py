2c3f72394714174c5a687f41db2cb3057fceabd1
Jan Eljasiak <102127797+JanEljasiak@users.noreply.github.com>
2026-07-02 01:25:52 +0200

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def softmax(input):
-    exp_list = np.array([np.exp(i) for i in input])
-    sum_exp_list = sum(exp_list)
-    return exp_list / sum_exp_list
-
-x = [1, 2, 8]
-print(softmax(x))
